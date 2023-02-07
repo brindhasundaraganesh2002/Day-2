690156a216adf76be2dd496fa300ddbe4ef1492c
brindhasundaraganesh2002 <90828734+brindhasundaraganesh2002@users.noreply.github.com>
2023-02-08 05:11:25 +0530

    AddLastDigits.java

---
+++ AddLastDigits.java
@@ -0,0 +1,7 @@
+public class AddLastDigits{
+    public int addLastDigits(int input1, int input2){
+        return Math.abs(input1 % 10) + Math.abs(input2 % 10);
+    }
+}
+Footer
+© 2023 GitHub, Inc.
